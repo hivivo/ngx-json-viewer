d3101cf5aec4d8e28ef105dd431514096aacc598
Vivo Xu <vivo@fancyber.com>
2017-07-12 22:57:32 -0700

    Begin parsing json

--- src/ngx-json-viewer/ngx-json-viewer.component.ts
+++ src/ngx-json-viewer/ngx-json-viewer.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnChanges, Input } from '@angular/core';
 
+interface Segment {
+  key: string;
+  value: any;
+  type: string;
+  description: string;
+  expanded: boolean;
+}
+
 @Component({
   selector: 'ngx-json-viewer',
   templateUrl: './ngx-json-viewer.component.html',
@@ -9,7 +17,25 @@ export class NgxJsonViewerComponent implements OnChanges {
 
   @Input() json: any;
 
-  constructor() { }
+  segments: Segment[] = [];
 
-  ngOnChanges() { }
+  ngOnChanges() {
+    if (typeof this.json === 'object') {
+      Object.keys(this.json).forEach( key => {
+        this.segments.push(this.parseKeyValue(key, this.json[key]));
+      });
+    }
+  }
+
+  private parseKeyValue(key: any, value: any): Segment {
+    const segment: Segment = {
+      key: key,
+      value: value,
+      type: undefined,
+      description: '' + value,
+      expanded: true
+    };
+
+    return segment;
+  }
 }
