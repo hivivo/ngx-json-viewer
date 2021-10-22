a8231e4945019a8a0ee91c0ac89f147471ee375b
Jonathan Henderson <jonathan.henderson@arria.com>
2021-10-22 15:38:09 +0100

    NSAT-3192 Add restore expanded segments functionality

--- src/ngx-json-viewer/ngx-json-viewer.component.ts
+++ src/ngx-json-viewer/ngx-json-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import {Component, OnChanges, Input, ViewChildren, QueryList}  from '@angular/core';
 
 export interface Segment {
   key: string;
@@ -14,16 +14,74 @@ export interface Segment {
   styleUrls: ['./ngx-json-viewer.component.scss']
 })
 export class NgxJsonViewerComponent implements OnChanges {
+  @Input() public json: any;
+  @Input() public expanded = true;
+  @Input() public depth = -1;
+  @Input() public key = 'Object';
+  // Tracks the length of array types. -1 for other types
+  @Input() public length = -1;
+  @Input() public restoreExpanded = false;
 
   @Input() json: any;
   @Input() expanded = true;
   @Input() depth = -1;
 
-  @Input() _currentDepth = -1;
+  @Input() public _currentDepth = -1;
+  @Input() public _previouslyOpenKeys?: {[key: string]: any};
 
-  segments: Segment[] = [];
+  public nextOpenKeys: {[key: string]: any} = {};
+  public segments: Segment[] = [];
 
-  ngOnChanges() {
+  // Matches the last underscore in a string
+  private underscoreRegex = /_[^_]+$/;
+
+  @ViewChildren(NgxJsonViewerComponent) public childrenComponents: QueryList<
+  NgxJsonViewerComponent
+  >;
+
+  private getOpenKeysRecursive(): any {
+    const openKeys: {[key: string]: any} = {};
+    this.childrenComponents.forEach((component : NgxJsonViewerComponent) => {
+      // Save key and length - on reload array elements should only be reopened if
+      // the array is the same length
+      openKeys[component.key + '_' + component.length] = component.getOpenKeysRecursive();
+    });
+
+    if (Object.keys(openKeys).length === 0) {
+      return;
+    }
+    return openKeys;
+  }
+
+  private openSegments(): void {
+    const keys = Object.keys(this._previouslyOpenKeys!);
+    keys.forEach(key => {
+      // Check to see if the key exists, if so expands it
+      const strippedKey = key.replace(this.underscoreRegex, '');
+      const foundSegment = this.segments.find(segment => {
+        return segment.key === strippedKey;
+      });
+
+      if (!foundSegment) {
+        return;
+      }
+
+      if (!this.isExpandable(foundSegment)) {
+        return;
+      }
+
+      foundSegment.expanded = true;
+    });
+  }
+
+  public ngOnChanges(): void {
+    // Save open keys structure before processing new json
+    // Will only run in top level
+    if (this.restoreExpanded && this.childrenComponents) {
+      this._previouslyOpenKeys = this.getOpenKeysRecursive();
+      console.log(this._previouslyOpenKeys);
+    }
+
     this.segments = [];
 
     // remove cycles
@@ -38,13 +96,19 @@ export class NgxJsonViewerComponent implements OnChanges {
     } else {
       this.segments.push(this.parseKeyValue(`(${typeof this.json})`, this.json));
     }
+
+    if (!this._previouslyOpenKeys) {
+      return;
+    } else {
+      this.openSegments();
+    }
   }
 
-  isExpandable(segment: Segment) {
+  public isExpandable(segment: Segment): any {
     return segment.type === 'object' || segment.type === 'array';
   }
 
-  toggle(segment: Segment) {
+  public toggle(segment: Segment): any {
     if (this.isExpandable(segment)) {
       segment.expanded = !segment.expanded;
     }
@@ -56,7 +120,7 @@ export class NgxJsonViewerComponent implements OnChanges {
       value: value,
       type: undefined,
       description: '' + value,
-      expanded: this.isExpanded()
+      expanded: this.isExpanded(),
     };
 
     switch (typeof segment.value) {
@@ -89,7 +153,8 @@ export class NgxJsonViewerComponent implements OnChanges {
           segment.description = 'null';
         } else if (Array.isArray(segment.value)) {
           segment.type = 'array';
-          segment.description = 'Array[' + segment.value.length + '] ' + JSON.stringify(segment.value);
+          segment.description =
+            'Array[' + segment.value.length + '] ' + JSON.stringify(segment.value);
         } else if (segment.value instanceof Date) {
           segment.type = 'date';
         } else {
@@ -104,51 +169,45 @@ export class NgxJsonViewerComponent implements OnChanges {
   }
 
   private isExpanded(): boolean {
-    return (
-      this.expanded &&
-      !(this.depth > -1 && this._currentDepth >= this.depth)
-    );
+    return this.expanded && !(this.depth > -1 && this._currentDepth >= this.depth);
   }
 
   // https://github.com/douglascrockford/JSON-js/blob/master/cycle.js
-  private decycle(object: any) {
+  private decycle(object: any): any {
     const objects = new WeakMap();
-    return (function derez(value, path) {
-      let old_path;
+    return (function derez(value: any, path: any): any {
+      let oldPath;
       let nu: any;
 
       if (
-        typeof value === 'object'
-        && value !== null
-        && !(value instanceof Boolean)
-        && !(value instanceof Date)
-        && !(value instanceof Number)
-        && !(value instanceof RegExp)
-        && !(value instanceof String)
+        typeof value === 'object' &&
+        value !== null &&
+        !(value instanceof Boolean) &&
+        !(value instanceof Date) &&
+        !(value instanceof Number) &&
+        !(value instanceof RegExp) &&
+        !(value instanceof String)
       ) {
-        old_path = objects.get(value);
-        if (old_path !== undefined) {
-          return {$ref: old_path};
+        oldPath = objects.get(value);
+        if (oldPath !== undefined) {
+          return {$ref: oldPath};
         }
         objects.set(value, path);
 
         if (Array.isArray(value)) {
           nu = [];
-          value.forEach(function (element, i) {
+          value.forEach((element, i) => {
             nu[i] = derez(element, path + '[' + i + ']');
           });
         } else {
           nu = {};
-          Object.keys(value).forEach(function (name) {
-            nu[name] = derez(
-              value[name],
-              path + '[' + JSON.stringify(name) + ']'
-            );
+          Object.keys(value).forEach(name => {
+            nu[name] = derez(value[name], path + '[' + JSON.stringify(name) + ']');
           });
         }
         return nu;
       }
       return value;
-    }(object, '$'));
+    })(object, '$');
   }
 }
