c9fd18e5ef202976d0d7516268c2ba12e43cfcc4
ron.b@lsports.eu <ron.b@lsports.eu>
2022-01-27 21:50:50 +0200

    feat: added search and mark text option
    passing a searchTerm will mark the text in the json

--- src/ngx-json-viewer/ngx-json-viewer.component.ts
+++ src/ngx-json-viewer/ngx-json-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, Input } from "@angular/core";
 
 export interface Segment {
   key: string;
@@ -6,20 +6,29 @@ export interface Segment {
   type: undefined | string;
   description: string;
   expanded: boolean;
+  markKey: boolean;
+  markValue: boolean;
 }
 
 @Component({
-  selector: 'ngx-json-viewer',
-  templateUrl: './ngx-json-viewer.component.html',
-  styleUrls: ['./ngx-json-viewer.component.scss']
+  selector: "ngx-json-viewer",
+  templateUrl: "./ngx-json-viewer.component.html",
+  styleUrls: ["./ngx-json-viewer.component.scss"],
 })
 export class NgxJsonViewerComponent implements OnChanges {
-
   @Input() json: any;
   @Input() expanded = true;
   @Input() depth = -1;
 
   @Input() _currentDepth = -1;
+
+  _searchTerm = "";
+  @Input() set searchTerm(value: string) {
+    this._searchTerm = value;
+    if (value?.length) {
+      this.segments = [...this.segments];
+    }
+  }
 
   segments: Segment[] = [];
 
@@ -31,17 +40,48 @@ export class NgxJsonViewerComponent implements OnChanges {
 
     this._currentDepth++;
 
-    if (typeof this.json === 'object') {
-      Object.keys(this.json).forEach(key => {
+    if (typeof this.json === "object") {
+      Object.keys(this.json).forEach((key) => {
         this.segments.push(this.parseKeyValue(key, this.json[key]));
       });
     } else {
-      this.segments.push(this.parseKeyValue(`(${typeof this.json})`, this.json));
+      this.segments.push(
+        this.parseKeyValue(`(${typeof this.json})`, this.json)
+      );
     }
   }
 
+  search() {
+    console.log("searchTerm ::: ", this._searchTerm);
+    console.log("segments ::: ", this.segments);
+    this.segments.forEach((segment) => {
+      if (segment.key.includes(this._searchTerm)) {
+        segment.markKey = true;
+      } else {
+        segment.markKey = false;
+      }
+
+      if (
+        typeof segment.value === "string" &&
+        segment.value.includes(this._searchTerm)
+      ) {
+        segment.markValue = true;
+      } else {
+        segment.markValue = false;
+      }
+    });
+    this.segments = [...this.segments];
+  }
+
+  isMarkedStyle(value: boolean) {
+    return {
+      // prettier-ignore
+      'mark': value,
+    };
+  }
+
   isExpandable(segment: Segment) {
-    return segment.type === 'object' || segment.type === 'array';
+    return segment.type === "object" || segment.type === "array";
   }
 
   toggle(segment: Segment) {
@@ -55,46 +95,58 @@ export class NgxJsonViewerComponent implements OnChanges {
       key: key,
       value: value,
       type: undefined,
-      description: '' + value,
-      expanded: this.isExpanded()
+      description: "" + value,
+      expanded: this.isExpanded(),
+      markKey:
+        this._searchTerm?.length > 0 &&
+        typeof key !== "object" &&
+        key.toString().includes(this._searchTerm),
+      markValue:
+        this._searchTerm?.length > 0 &&
+        typeof value !== "object" &&
+        value.toString().includes(this._searchTerm),
     };
 
     switch (typeof segment.value) {
-      case 'number': {
-        segment.type = 'number';
+      case "number": {
+        segment.type = "number";
         break;
       }
-      case 'boolean': {
-        segment.type = 'boolean';
+      case "boolean": {
+        segment.type = "boolean";
         break;
       }
-      case 'function': {
-        segment.type = 'function';
+      case "function": {
+        segment.type = "function";
         break;
       }
-      case 'string': {
-        segment.type = 'string';
+      case "string": {
+        segment.type = "string";
         segment.description = '"' + segment.value + '"';
         break;
       }
-      case 'undefined': {
-        segment.type = 'undefined';
-        segment.description = 'undefined';
+      case "undefined": {
+        segment.type = "undefined";
+        segment.description = "undefined";
         break;
       }
-      case 'object': {
+      case "object": {
         // yea, null is object
         if (segment.value === null) {
-          segment.type = 'null';
-          segment.description = 'null';
+          segment.type = "null";
+          segment.description = "null";
         } else if (Array.isArray(segment.value)) {
-          segment.type = 'array';
-          segment.description = 'Array[' + segment.value.length + '] ' + JSON.stringify(segment.value);
+          segment.type = "array";
+          segment.description =
+            "Array[" +
+            segment.value.length +
+            "] " +
+            JSON.stringify(segment.value);
         } else if (segment.value instanceof Date) {
-          segment.type = 'date';
+          segment.type = "date";
         } else {
-          segment.type = 'object';
-          segment.description = 'Object ' + JSON.stringify(segment.value);
+          segment.type = "object";
+          segment.description = "Object " + JSON.stringify(segment.value);
         }
         break;
       }
@@ -105,8 +157,7 @@ export class NgxJsonViewerComponent implements OnChanges {
 
   private isExpanded(): boolean {
     return (
-      this.expanded &&
-      !(this.depth > -1 && this._currentDepth >= this.depth)
+      this.expanded && !(this.depth > -1 && this._currentDepth >= this.depth)
     );
   }
 
@@ -118,37 +169,37 @@ export class NgxJsonViewerComponent implements OnChanges {
       let nu: any;
 
       if (
-        typeof value === 'object'
-        && value !== null
-        && !(value instanceof Boolean)
-        && !(value instanceof Date)
-        && !(value instanceof Number)
-        && !(value instanceof RegExp)
-        && !(value instanceof String)
+        typeof value === "object" &&
+        value !== null &&
+        !(value instanceof Boolean) &&
+        !(value instanceof Date) &&
+        !(value instanceof Number) &&
+        !(value instanceof RegExp) &&
+        !(value instanceof String)
       ) {
         old_path = objects.get(value);
         if (old_path !== undefined) {
-          return {$ref: old_path};
+          return { $ref: old_path };
         }
         objects.set(value, path);
 
         if (Array.isArray(value)) {
           nu = [];
           value.forEach(function (element, i) {
-            nu[i] = derez(element, path + '[' + i + ']');
+            nu[i] = derez(element, path + "[" + i + "]");
           });
         } else {
           nu = {};
           Object.keys(value).forEach(function (name) {
             nu[name] = derez(
               value[name],
-              path + '[' + JSON.stringify(name) + ']'
+              path + "[" + JSON.stringify(name) + "]"
             );
           });
         }
         return nu;
       }
       return value;
-    }(object, '$'));
+    })(object, "$");
   }
 }
